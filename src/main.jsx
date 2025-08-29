c7fe4771649f951f07f7e77b0474e7f776d44ac4
Dea Inesia Sri Utami <deainesia223@gmail.com>
2025-08-29 11:13:56 +0700

    Update basename

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/handsip">
     <App />
   </BrowserRouter>,
 );
